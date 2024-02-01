d0166b134e7c5b278110b4232e0acfd75fdcf748
Joseph Dicarlo <dic@rlo.io>
2024-01-31 19:37:20 -0500

    fix: get env vars at runtime

--- src/routes/api/rcon/+server.ts
+++ src/routes/api/rcon/+server.ts
@@ -1,14 +1,35 @@
-import { RCON_HOSTNAME } from '$env/static/private';
-import { RCON_PORT } from '$env/static/private';
-import { RCON_PASSWORD } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import { RconConnection } from '$lib/server/connection';
 import { json } from '@sveltejs/kit';
+
+function logAndExit(message: string, code: number = 1): never {
+  console.error(message);
+  return process.exit(code);
+}
+
+function getEnv(name: string): string {
+  const value = env[name];
+  if (!value) logAndExit(`${name} must be set`);
+  return value;
+}
+
+function getEnvNumber(name: string): number {
+  try {
+    return Number(getEnv(name));
+  } catch (_) {
+    logAndExit(`${name} must be a number (1-65535)`);
+  }
+}
+
+const RCON_HOSTNAME = getEnv('RCON_HOSTNAME');
+const RCON_PORT = getEnvNumber('RCON_PORT');
+const RCON_PASSWORD = getEnv('RCON_PASSWORD');
 
 const rconConnection = new RconConnection();
 
 export async function GET() {
-    await rconConnection.connect(RCON_HOSTNAME, Number(RCON_PORT), RCON_PASSWORD);
   if (!rconConnection.connected) {
+    await rconConnection.connect(RCON_HOSTNAME, RCON_PORT, RCON_PASSWORD);
   }
 
   const info = (await rconConnection.exec('Info')).body;
